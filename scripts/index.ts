f4feb7ceb55cf83e0f815f23da441688e1b05686
micronoyau <leo.lf.dk@gmail.com>
2023-12-07 17:24:49 +0100

    Working now

---
+++ scripts/index.ts
@@ -0,0 +1,45 @@
+import { log } from "./logger.js";
+import { createReadStream, createWriteStream } from "./frida-fs.js";
+import { Buffer } from "buffer";
+
+Java.perform(function () {
+    var initialClasses = []
+
+    Java.enumerateLoadedClasses({
+        onMatch: function (className) {
+            initialClasses.push(className);
+        },
+        onComplete: function () {
+            console.log("Loaded initial classes");
+        }
+    });
+
+    const PathClassLoader = Java.use('dalvik.system.PathClassLoader');
+    const init = PathClassLoader.$init.overload('java.lang.String', 'java.lang.ClassLoader');
+
+    init.implementation = function (dexPath, classLoader) {
+        console.log("Loaded " + dexPath);
+
+        var readStream = createReadStream(dexPath)
+        var dex_content = ""
+
+        readStream
+            .on('readable', function () {
+                var chunk;
+                while (null !== (chunk = readStream.read())) {
+                    dex_content = dex_content.concat(chunk);
+                }
+            })
+            .on('end', function () {
+                const buf = Buffer.from(dex_content);
+                const writeStream = createWriteStream('/data/user/0/com.example.ut_dyn_load/test.dex');
+                writeStream.write(buf);
+                writeStream.end();
+
+                log("Saved dex file :)");
+            });
+
+        return init.call(this, dexPath, classLoader);
+    }
+})
+
