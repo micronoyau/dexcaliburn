532c91a0e6aa93a1e9d4e5b972aa311b2774f602
Devilsharu <sharus8@yahoo.fr>
2023-12-19 15:07:24 +0100

    update python script

--- python/server.py
+++ python/server.py
@@ -3,14 +3,14 @@ import subprocess
 import re
 import os
 
-proc = subprocess.Popen(["frida", "-U", "-f" "com.example.ut_dyn_load", "-l", "out/_script.js"], stdout=subprocess.PIPE)
 classNameFile = open("dex-files/classNames.txt","w")
+os.system("mkdir -p dex-files")
+proc = subprocess.Popen(["frida", "-U", "-f" "com.example.ut_dyn_load", "-l", "out/_script.js"], stdout=subprocess.PIPE)
 
 for line in io.TextIOWrapper(proc.stdout, encoding="utf-8"):  # or another encoding
     pathDexFile = re.search("(?<=LOADEDDEXFILE).*(?=ENDDEXFILE)", line)
     className = re.search("(?<=CLASS).*(?=ENDCLASS)", line)
     if pathDexFile:
-        os.system("mkdir -p dex-files")
         os.system("adb pull " + pathDexFile.group(0) + " dex-files/")
         os.system("adb shell rm " + pathDexFile.group(0))
     if className:
