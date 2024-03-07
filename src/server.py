97233359a3fc051afe2803fe2c3461053b517178
micronoyau <leo.lf.dk@gmail.com>
2024-03-07 10:40:11 +0100

    chore: server interface

--- src/server.py
+++ src/server.py
@@ -2,6 +2,7 @@ import frida
 import time
 import os
 from utils import *
+from sys import argv
 
 os.system(f"mkdir -p {OUTPUT_FOLDER}")
 os.system(f"mkdir -p {LOG_FOLDER}")
@@ -42,20 +43,27 @@ def message_handler(message, data):
                 break
 
 
-device = frida.get_usb_device()
-pid = device.spawn(["com.example.ut_dyn_load"])
-session = device.attach(pid)
+if __name__ == '__main__':
+    device = frida.get_usb_device()
 
-try:
-    f = open(FRIDA_SCRIPT, 'r')
+    if len(argv) == 2:
+        pid = device.spawn([argv[1]])
+    elif len(argv) == 1:
+        pid = device.spawn(["com.example.ut_dyn_load"])
+    else:
+        exit(-1)
 
-except FileNotFoundError:
-    panic("Unable to find frida script")
+    session = device.attach(pid)
 
-script = session.create_script(f.read())
-script.on("message", message_handler)  # register the message handler
-script.load()
-device.resume(pid)
+    try:
+        f = open(FRIDA_SCRIPT, 'r')
 
-# Temporary : 5 seconds before killing the server
-time.sleep(5)
+    except FileNotFoundError:
+        panic("Unable to find frida script")
+
+    script = session.create_script(f.read())
+    script.on("message", message_handler)  # register the message handler
+    script.load()
+    device.resume(pid)
+
+    input()
