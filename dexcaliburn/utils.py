85ebbac4462021dbc008959e8fc0a1fd958a5f99
Jean-Marie Mineau <jean-marie.mineau@inria.fr>
2024-04-04 11:49:13 +0200

    somewhat package this

--- dexcaliburn/utils.py
+++ dexcaliburn/utils.py
@@ -1,19 +1,23 @@
 import json
+from pathlib import Path
 
 DEBUG = True
-DEX_FOLDER = 'dex-files'
-LOG_FOLDER = 'logs'
-FRIDA_SCRIPT = 'src/frida-scripts/out/_script.js'
-HOOK_CONFIG_FILE = 'hooks.config'
+DEX_FOLDER = "dex-files"
+LOG_FOLDER = "logs"
+FRIDA_SCRIPT = Path(__file__).parent / "frida-scripts" / "out" / "_script.js"
+HOOK_CONFIG_FILE = "hooks.config"
+
 
 def panic(msg: str):
     print("PANIC")
     print(msg)
     exit(1)
 
+
 def printd(msg):
     if DEBUG:
         print(msg)
+
 
 def dump_json(json_data):
     return json.dumps(json_data, indent=2)
